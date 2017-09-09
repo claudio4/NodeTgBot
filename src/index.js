e2b4992d3048aa0d2201913ab7d2d32de218a8a4
Claudio4 <me@claudio4.com>
2017-09-09 01:41:39 +0100

    Repo init

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const teleapi = require('teleapi');
+
+module.exports = class Bot {
+  constructor(token, chats, pollingRatio) {
+    this.api = teleapi(token);
+    this.chats = chats;
+    this.pollingRatio = pollingRatio;
+    this.offset = null;
+    this.commands = [];
+  }
+  addCommand(regex, handler) {
+    this.commands[this.commands.length] = (update) => {
+      if (regex.test(update.text)) {
+        handler(update.message, this);
+      }
+    };
+
+    return this;
+  }
+  sendMessage(msg) {
+    this.api.sendMessage(msg);
+  }
+  checkForUpdates() {
+    this.api.getUpdates({ offset: this.offset })
+      .then((result) => {
+        result.forEach((update) => {
+          this.commands.forEach(command => command(update));
+          this.offset = update.update_id + 1;
+        });
+      });
+  }
+  Poll() {
+    this.checkForUpdates();
+    setTimeout(this.Poll.bind(this), this.pollingRatio);
+  }
+  startPolling() {
+    (this.Poll.bind(this))();
+  }
+};
